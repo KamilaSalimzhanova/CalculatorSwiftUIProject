2c2b5dd3def940c7a974d75548d69d417f736dd4
kamila <ksalimzh@gmail.com>
2025-03-21 21:42:00 +0500

    AnyAppearance - lightMode, dark - darkMode, uploaded Colors

---
+++ CalculatorSwiftUIProject/Constants.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+let buttonBackgroundColor = Color("ButtonBackground")
+let digitsColor = Color("DigitsColor")
+let rightButtonsColor = Color("LightRed")
+let topButtonsColor = Color("LightGreen")
+let backgroundColor = Color("LightWhite")
+let secondaryBackgroundColor = Color("AllButtonsBackground")

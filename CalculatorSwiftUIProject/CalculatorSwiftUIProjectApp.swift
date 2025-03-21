4b7b449e13f880bbbb7cfda1bc3666033b7d17f0
kamila <ksalimzh@gmail.com>
2025-03-21 21:14:21 +0500

    Initial Commit

---
+++ CalculatorSwiftUIProject/CalculatorSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIProjectApp.swift
+//  CalculatorSwiftUIProject
+//
+//  Created by kamila on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
